d7491059456ba8e091bf96f04d074694860a9d08
Jordi Pont-Tuset <jponttuset@gmail.com>
2015-05-15 09:14:42 +0200

    - Scripts to get partitions out of UCMs added

---
+++ src/scripts/extract_all_partitions_ucms.m
@@ -0,0 +1,24 @@
+function extract_all_partitions_ucms(method,gt_set)
+% extract_all_partitions_ucms(method,gt_set)
+% ------------------------------------------------------------------------ 
+%  Copyright (C)
+%  Universitat Politecnica de Catalunya BarcelonaTech (UPC) - Spain
+% 
+%  Jordi Pont-Tuset <jordi.pont@upc.edu>
+%  March 2011
+% ------------------------------------------------------------------------ 
+if nargin==1
+    gt_set = 'val';
+end
+
+% Get thresholds
+n_th = 30;
+params = cell(n_th,1);
+for ii=1:n_th
+    params{ii} = ii/31;
+end
+
+% Run for all thresholds
+for jj=1:length(params)  
+    extract_partitions_ucms(method,gt_set, str2double(params{jj}))
+end
---
+++ src/scripts/extract_partitions_ucms.m
@@ -0,0 +1,31 @@
+function extract_partitions_ucms(method, gt_set, parameter)
+% extract_partitions_ucms(method, gt_set, parameter)
+% ------------------------------------------------------------------------ 
+%  Copyright (C)
+%  Universitat Politecnica de Catalunya BarcelonaTech (UPC) - Spain
+% 
+%  Jordi Pont-Tuset <jordi.pont@upc.edu>
+%  March 2011
+% ------------------------------------------------------------------------ 
+im_ids = load(fullfile(root_dir,'bsds500', ['ids_' gt_set '.txt']));
+
+method_dir = fullfile(datasets_root_dir, 'BSDS500', method);
+parts_dir = fullfile(datasets_root_dir, 'BSDS500', method, num2str(parameter));
+if ~exist(parts_dir,'dir')
+    mkdir(parts_dir)
+end
+
+for ii=1:numel(im_ids)
+    % Display evolution      
+    disp([num2str(ii) ' out of ' num2str(numel(im_ids))])
+        
+    % Get the partition to assess
+    load(fullfile(method_dir, [im_ids{ii} '.mat']));
+    tmp = (bwlabel(ucm2'<=parameter,4))';
+    partition = uint32(tmp(2:2:end,2:2:end));
+   
+    % Write to file
+    part_file = fullfile(parts_dir, [im_ids{ii} '_' num2str(parameter) '.prl']);
+    prl_write(partition, part_file);
+end
+
