39ec562270d01c088c7968acf09571d55c60041f
Jordi Pont-Tuset <jponttuset@gmail.com>
2016-01-20 16:06:40 +0100

    - Evaluating contour detections using Piotr's edges toolbox

--- install.m
+++ install.m
@@ -21,21 +21,6 @@
 % The path below corresponds to the folder where the package is installed.
 % ------------------------------------------------------------------------ 
 
-% Set the dependencies path
-segbench_path = '/Users/jpont/Workspace/libs/segbench/Benchmark';
-
-% Install BSDS300 segbench code
-if exist(segbench_path,'dir')==0
-    disp('!!! Warning !!!')
-    disp('BSDS300 segbench path not found: You will not be able to compute the precision-recall for boundaries measure')
-else
-    addpath(genpath(segbench_path));
-    if exist('correspondPixels')~=3 %#ok<EXIST>
-        disp('!!! Warning !!!')
-        disp('The needed function (correspondPixels) not found in segbench. Have you compiled it properly?')
-    end
-end
-
 % Check that 'root_dir' has been set
 if ~exist(root_dir,'dir')
     error('Error installing the package, try updating the value of root_dir in the file "root_dir.m"')
@@ -56,6 +41,9 @@ addpath(fullfile(root_dir,'cvpr2013'));
 addpath(genpath(fullfile(root_dir,'src')));
 
 % Check that the needed functions are compiled
+if exist('correspondPixels')~=3 %#ok<EXIST>
+    disp('The needed function (correspondPixels) not found in piotr_edges.')
+end
 if exist('mex_eval_segm')~=3 %#ok<EXIST>
     error('The needed function (mex_eval_segm) not found. Have you build the package properly?')
 end
