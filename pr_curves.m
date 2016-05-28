7655c861e0aafeee5865ec16b13094cf23fe402d
Kevis Maninis <kevismaninis@hotmail.com>
2016-05-28 20:48:14 +0200

    reproduced all results, interfaced PASCALContext and Pascal, aux->misc, changed pr_curves and pr_curves_to_file

--- pr_curves.m
+++ pr_curves.m
@@ -1,10 +1,10 @@
-% ------------------------------------------------------------------------ 
+% ------------------------------------------------------------------------
 %  Copyright (C)
 %  Universitat Politecnica de Catalunya BarcelonaTech (UPC) - Spain
-% 
+%
 %  Jordi Pont-Tuset <jordi.pont@upc.edu>
 %  June 2013
-% ------------------------------------------------------------------------ 
+% ------------------------------------------------------------------------
 %  Code obtained from:
 %  https://imatge.upc.edu/web/resources/supervised-evaluation-image-segmentation
 % ------------------------------------------------------------------------
@@ -13,12 +13,12 @@
 %    "Measures and Meta-Measures for the Supervised Evaluation of Image Segmentation,"
 %    Computer Vision and Pattern Recognition (CVPR), 2013.
 % If you use this code, please consider citing the paper.
-% ------------------------------------------------------------------------ 
+% ------------------------------------------------------------------------
 %
 % To evaluate your method in the precision-recall enviroments you have to
 % follow the following steps:
 %
-%  1) Create a file in the folder "parameters" with name "mymethod.txt" 
+%  1) Create a file in the folder "parameters" with name "mymethod.txt"
 %     with all the different value of a parameter indexing different working
 %     points of your method, sorted as wished. (e.g. UCM thresholds)
 %  2) Create a folder inside "datasets" with a descriptive name e.g. "mymethod".
@@ -37,56 +37,100 @@
 %     the list 'which_contours'.
 %  6) Add your method to the list of methods to display, run the rest of
 %     the code and enjoy your PR curves!
-% ------------------------------------------------------------------------ 
+% ------------------------------------------------------------------------
 
 %% Experiments parameters
-% BSDS500 set: train, test, val, trainval, or all
-% database = 'Pascal';
-% gt_set   = 'Segmentation_val_2012';
-database = 'BSDS500';
-gt_set   = 'test';
+%clear all;close all;clc;
+
+
+%database = 'BSDS500';
+database = 'PASCALContext';
+%database = 'Pascal';
+%database = 'SBD';
+
+writePR = 1; % Write results in format to use latex code?
+USEprecomputed = 1; % Use precomputed results or evaluate on your computer?
 
 % Precision-recall measures
-measures = {'fb' ... % Precision-recall for boundaries
-            'fop',... % Precision-recall for objects and parts  
-            };
-              
+measures = {'fb',... % Precision-recall for boundaries
+            'fop'};   % Precision-recall for Object Proposals
+
 % Define all methods to be compared
 methods  = [];
-methods(end+1).name = 'LEP'     ; methods(end).io_func = @read_one_lep;
-methods(end+1).name = 'MCG'     ; methods(end).io_func = @read_one_ucm;
-methods(end+1).name = 'gPb-UCM' ; methods(end).io_func = @read_one_ucm;
-methods(end+1).name = 'ISCRA'   ; methods(end).io_func = @read_one_ucm;
-methods(end+1).name = 'NWMC'    ; methods(end).io_func = @read_one_ucm;
-methods(end+1).name = 'IIDKL'   ; methods(end).io_func = @read_one_ucm;
-methods(end+1).name = 'EGB'     ; methods(end).io_func = @read_one_prl;
-methods(end+1).name = 'MShift'  ; methods(end).io_func = @read_one_prl;
-methods(end+1).name = 'NCut'    ; methods(end).io_func = @read_one_prl;
-methods(end+1).name = 'QuadTree'; methods(end).io_func = @read_one_prl;
+switch database,
+    case 'BSDS500',
+        gt_set   = 'test';
+        methods(end+1).name = 'HED';             methods(end).io_func = @read_one_cont_png; methods(end).legend = methods(end).name;  methods(end).type = 'contour';
+       %methods(end+1).name = 'COB';             methods(end).io_func = @read_one_ucm;      methods(end).legend = 'COB';              methods(end).type = 'segmentation';
+        
+        %% Old methods...
+        methods(end+1).name = 'LEP';             methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        methods(end+1).name = 'MCG';             methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        methods(end+1).name = 'SE';              methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'contour';
+        methods(end+1).name = 'MShift';          methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        methods(end+1).name = 'gPb-UCM';         methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        methods(end+1).name = 'ISCRA';           methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        methods(end+1).name = 'NCut';            methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        methods(end+1).name = 'EGB';             methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        
+        
+    case 'PASCALContext',
+        gt_set   = 'test_new';
+        
+        methods(end+1).name = 'COB';             methods(end).io_func = @read_one_ucm;      methods(end).legend = 'COB train';        methods(end).type = 'segmentation';
+        methods(end+1).name = 'COB_trainval';    methods(end).io_func = @read_one_ucm;      methods(end).legend = 'COB trainval';     methods(end).type = 'segmentation'; 
+        methods(end+1).name = 'HED';             methods(end).io_func = @read_one_cont_png; methods(end).legend = methods(end).name;  methods(end).type = 'contour';
+        methods(end+1).name = 'HED_trainval';    methods(end).io_func = @read_one_cont_png; methods(end).legend = 'HED trainval';     methods(end).type = 'contour';
+        methods(end+1).name = 'HED-BSDS500';     methods(end).io_func = @read_one_cont_png; methods(end).legend = methods(end).name;  methods(end).type = 'contour';
+        methods(end+1).name = 'LEP-BSDS500';     methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        methods(end+1).name = 'MCG-BSDS500';     methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        methods(end+1).name = 'SE-BSDS500';              methods(end).io_func = @read_one_cont_png; methods(end).legend = methods(end).name; methods(end).type = 'contour';
+        
+    case 'Pascal',
+        gt_set   = 'Segmentation_val_2012';
+        
+        methods(end+1).name = 'COB';             methods(end).io_func = @read_one_ucm;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        methods(end+1).name = 'HED';             methods(end).io_func = @read_one_cont_png; methods(end).legend = methods(end).name;  methods(end).type = 'contour'; %From Khoreva et al.
+        methods(end+1).name = 'HED-BSDS500';     methods(end).io_func = @read_one_cont_png; methods(end).legend = methods(end).name;  methods(end).type = 'contour';
+        methods(end+1).name = 'MCG-BSDS500';     methods(end).io_func = @read_one_lep;      methods(end).legend = methods(end).name;  methods(end).type = 'segmentation';
+        
+    case 'SBD',
+        gt_set   = 'val';
+        cat_id = 15;
+        methods(end+1).name = 'HED';            methods(end).io_func = @read_one_cont_png; methods(end).legend = methods(end).name; methods(end).type = 'contour';
+        methods(end+1).name = 'COB';            methods(end).io_func = @read_one_ucm; methods(end).legend = 'COB'; methods(end).type = 'segmentation';
+    otherwise,
+        error('Unknown name of the database');
+end
 
-% Which of these are only contours?
-which_contours = {''};
-
-% Colors to display    
-colors = {'k','g','b','r','m','c','y','r','k','g','b'};
+% Colors to display
+colors = {'b','g','r','k','m','c','y','b','g','r','k','m','c','y','k','g','b','g','r'};
 
 %% Evaluate your method (just once for all parameters)
 
-% Evaluate using the correct reading function
-% for ii=1:length(measures)
-%     for jj=1:length(methods)
-%         % Contours only in 'fb'
-%         is_cont = any(ismember(which_contours,methods(ii).name));
-%         if strcmp(measures{ii},'fb') || ~is_cont
-%             eval_method_all_params(methods(jj).name, measures{ii}, methods(jj).io_func, database, gt_set, is_cont)
-%         end
-%     end
-% end
+% % Evaluate using the correct reading function
+if ~USEprecomputed,
+    for ii=1:length(measures)
+        for jj=1:length(methods)
+            % Contours only in 'fb'
+            is_cont = strcmp(methods(ii).type,'contour');
+            if strcmp(measures{ii},'fb') || ~is_cont
+                if exist('cat_id','var'),
+                    eval_method_all_params(methods(jj).name, measures{ii}, methods(jj).io_func, database, gt_set, is_cont,cat_id);
+                else
+                    eval_method_all_params(methods(jj).name, measures{ii}, methods(jj).io_func, database, gt_set, is_cont);
+                end
+            end
+        end
+    end
+end
 
-% % Helper to check existence of files
+% Helper to check existence of files
 % test_io(methods, database, gt_set);
 
 %% Plot PR curves
+addpath(genpath('src'));
+
 for kk=1:length(measures)
     iso_f_axis(measures{kk})
     fig_handlers = [];
@@ -106,25 +150,35 @@ for kk=1:length(measures)
     
     % Plot methods
     for ii=1:length(methods)
-        
+        display(['Evaluating method: ' methods(ii).name '...']);
         % Plot the contours only in 'fb'
-        if strcmp(measures{kk},'fb') || all(~ismember(which_contours,methods(ii).name))
+        if strcmp(measures{kk},'fb') || strcmp(methods(ii).type,'segmentation')
+            if strcmp(methods(ii).type,'contour'),style='-';else style='-';end
+            
             % Get all parameters for that method from file
             params = get_method_parameters(methods(ii).name);
-
+            
             % Gather pre-computed results
             curr_meas = gather_measure(methods(ii).name,params,measures{kk},database,gt_set);
-            curr_ods = general_ods(curr_meas);
-            curr_ois = general_ois(curr_meas);
-
+            curr_ods  = general_ods(curr_meas);
+            curr_ois  = general_ois(curr_meas);
+            curr_ap   = general_ap(curr_meas);
             % Plot method
-            fig_handlers(end+1) = plot(curr_meas.mean_rec,curr_meas.mean_prec,[colors{ii} '-']); %#ok<SAGROW>
-            plot(curr_ods.mean_rec,curr_ods.mean_prec,[colors{ii} '*'])
-            legends{end+1} = [methods(ii).name ' [' sprintf('%0.3f',curr_ods.mean_value) '-' sprintf('%0.3f',curr_ois.mean_value) ']']; %#ok<SAGROW>
+            display(['ODS: ' num2str(curr_ods.mean_value) ' OIS: ' num2str(curr_ois.mean_value) ' AP: ' num2str(curr_ap) ])
+            fig_handlers(end+1) = plot(curr_meas.mean_rec,curr_meas.mean_prec,[colors{ii} style],'LineWidth',2); %#ok<SAGROW>
+            plot(curr_ods.mean_rec,curr_ods.mean_prec,[colors{ii} '*'],'LineWidth',2)
+            legends{end+1} = ['[odsF:' sprintf('%0.3f',curr_ods.mean_value) ' oisF:' sprintf('%0.3f',curr_ois.mean_value) ' AP:' sprintf('%0.3f',curr_ap) '] ' methods(ii).legend ]; %#ok<SAGROW>
         end
     end
     
+    if strfind(gt_set,'_'), set_title = strrep(gt_set,'_','\_');else set_title=gt_set;end
+    
+    title([measures{kk} ' ' database ' ' set_title]);
     legend(fig_handlers,legends, 'Location','NorthEastOutside')
 end
 
+%Write the results for latex processing
+if writePR,
+    pr_curves_to_file(measures, database, gt_set, methods)
+end
 
--- src/gt_wrappers/db_root_dir.m
+++ src/gt_wrappers/db_root_dir.m
@@ -12,10 +12,8 @@
 % Please consider citing the paper if you use this code.
 % ------------------------------------------------------------------------
 function db_root_dir = db_root_dir( database )
-if strcmp(database,'Pascal') || strcmp(database,'COCO') ||  strcmp(database,'SBD') ||  strcmp(database,'ILSVRC') ||  strcmp(database,'BSDS500') || strcmp(database,'bsds_object_gt')
+if strcmp(database,'Pascal') || strcmp(database,'PASCALContext') || strcmp(database,'COCO') ||  strcmp(database,'SBD') ||  strcmp(database,'ILSVRC') ||  strcmp(database,'BSDS500') || strcmp(database,'bsds_object_gt')
     db_root_dir = fullfile('/srv/glusterfs/jpont/gt_dbs/',database);
-elseif strcmp(database,'PASCALContext'),
-    db_root_dir = fullfile('/srv/glusterfs/kmaninis/Databases/Boundary_Detection/',database);
 else
     error(['Unknown database: ' database]);
 end
